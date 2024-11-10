4aad8ec76083def456b97fd12288a53c3177e8df
mariaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <ksiar1212022@feit.ukim.edu.mk>
2024-11-10 15:42:16 +0100

    committed

--- проект #1/script.js
+++ проект #1/script.js
@@ -43,8 +43,8 @@
            "model": "Lincoln Futura 1955 (Batmobile)",
            "description": "The Lincoln Futura, a concept car built in 1955, became world-famous as the Batmobile in the 1960's Batman series. With its futuristic design, bubble canopy and jet-inspired tailfins, it captures the imaginations of car lovers and Batman fans alike.",
            "price": "$4.62 million",
-           "carInfo": "astonmartindb5.html",
-           "reviewLink": "astonmartindb.html",
+           "carInfo": "https://en.wikipedia.org/wiki/Lincoln_Futura",
+           "reviewLink": "https://www.cardesignnews.com/concept-car-of-the-week-lincoln-futura-1955/38048.article",
            "image":"batmobile.png",
            "likes": 90,
            "comments": []
@@ -64,8 +64,8 @@
            "model": "Ecto-1 (Ghostbusters)",
            "description": "The Ecto-1 is a 1959 Cadillac ambulance turned ghost-hunting vehicle used by the Ghostbusters in the beloved 1984 movie. Featuring a large siren, ghost-catching equipment, and the unmistakable logo, the Ecto-1 became an essential character in the franchise. Its classic vintage look and powerful presence continue to make it an iconic symbol of the Ghostbusters' adventure.",
            "price": "$150,000 ",
-           "carInfo": "astonmartindb5.html",
-           "reviewLink": "https://screenrant.com/ecto-1-ghostbusters-vehicle-history/",
+           "carInfo": "https://fraserengineco.com/fraser-fast-facts/ecto-1/",
+           "reviewLink": "https://truenorthbricks.com/2020/11/05/ghostbusters-ecto-1-10274-review/",
            "image":"ecto-1.png",
            "likes": 8,
            "comments": []
@@ -74,8 +74,8 @@
            "model": "Ferrari 250 GT California (Ferris Bueller's Day Off)",
            "description": "The Ferrari 250 GT California is one of the most expensive and iconic cars featured in the 1986 movie *Ferris Bueller's Day Off*. Driven by Ferris and his friends during their day of adventure in Chicago, the car became an instant classic. Known for its elegant design, luxury, and powerful V12 engine, this Ferrari is a symbol of wealth and style.",
            "price": "$18 million ",
-           "carInfo": "astonmartindb5.html",
-           "reviewLink": "https://www.motortrend.com/news/ferrari-250-gt-california-rare-cars/",
+           "carInfo": "https://en.wikipedia.org/wiki/Ferrari_250_GT_California_Spyder",
+           "reviewLink": "https://robbreport.com/motors/cars/first-drive-gto-engineering-replica-of-ferrari-250-gt-swb-california-spyder-1234638004/",
            "image":"ferrari250.png",
            "likes": 100,
            "comments": ["Cena?"]
@@ -113,11 +113,11 @@
 
    var json2 = { "items2":[
     {
-        "model": "Dean's Chevy Impala 1967 (Supernatural)",
+        "model": "Dean's Chevrolet Impala 1967 (Supernatural)",
         "description": "Dean Winchester's 1967 Chevy Impala is one of the most iconic vehicles in TV history. With its dark, ominous appearance, it served as the main transportation for the Winchester brothers in their fight against supernatural forces. The black muscle car is equipped with an arsenal of weapons and has witnessed countless battles and emotional moments.",
         "price": "$50,000",
-        "carInfo": "scoobvan.html",
-        "reviewLink": "https://screenrant.com/supernatural-deans-impala-history-facts/",
+        "carInfo": "https://www.conceptcarz.com/s7220/chevrolet-impala-series.aspx",
+        "reviewLink": "https://www.cars.com/research/chevrolet-impala-1967/consumer-reviews/",
         "image":"chevy.png",
         "likes": 0,
         "comments": []
@@ -136,8 +136,8 @@
         "model": " Honda S2000 (Suki's Car)",
         "description": "Suki's custom pink Honda S2000 from '2 Fast 2 Furious' is a standout in the Fast & Furious franchise. Known for its sleek design, bright pink color, and neon accents, this car reflects Suki’s bold and fearless personality. It's a symbol of speed, style, and defiance on the streets.",
         "price": "$40,000",
-        "carInfo": "scoobvan.html",
-        "reviewLink": "https://www.motortrend.com/cars/honda/s2000/2000-fast-furious-legacy/",
+        "carInfo": "https://en.wikipedia.org/wiki/Honda_S2000",
+        "reviewLink": "https://www.pistonheads.com/news/ph-driven/honda-s2000--ph-used-review/42038",
         "image":"suki.png",
         "likes": 26,
         "comments": [] 
@@ -147,8 +147,8 @@
         "model": "DeLorean (Back to the Future)",
         "description": "The DeLorean, modified with time-travel capabilities by Doc Brown in 'Back to the Future,' is one of the most famous cars in cinema history. Its stainless steel body, gull-wing doors, and futuristic design, combined with the flux capacitor, make it a symbol of adventure, time travel, and 80s nostalgia.",
         "price": "$25,000 ",
-        "carTnIo": "scoobvan.html",
-        "reviewLink": "https://www.autobahnautomotive.com/blog/back-to-the-future-delorean/",
+        "carTnIo": "https://en.wikipedia.org/wiki/DMC_DeLorean",
+        "reviewLink": "https://www.thedrive.com/new-cars/42094/i-drove-a-1981-delorean-and-could-not-stop-smiling",
         "image":"delorean.png",
         "likes": 0,
         "comments": []
@@ -157,8 +157,8 @@
         "model": "1986 Fleetwood Bounder RV (The Krystal Ship - Breakin Bad)",
         "description": "Own a piece of TV history with this replica of the iconic RV from Breaking Bad. Known as the Krystal Ship, this 1986 Fleetwood Bounder served as Walter White and Jesse Pinkman’s mobile meth lab. With its weathered look and classic design, it’s a must-have for fans wanting a memorable piece of the show’s legacy.",
         "price": "$65,000 ",
-        "carInfo": "scoobvan.html",
-        "reviewLink": "https://www.jackery.com/blogs/knowledge/breaking-bad-rv",
+        "carInfo": "https://www.jdpower.com/rvs/1986/bounder/m-34sb/3003094/specs",
+        "reviewLink": "https://vanlivingforum.com/threads/opinions-on-the-fleetwood-bounder-and-other-class-as.10009/",
         "image":"bbvan.png",
         "likes": 0,
         "comments": ["Dali doagja so opremata?"]
