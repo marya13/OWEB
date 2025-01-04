90866396d5e7ba084a1a4592bf1907b601ea3306
mariaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <ksiar1212022@feit.ukim.edu.mk>
2025-01-04 23:19:04 +0100

    so aria

--- angular/test/src/app/app.component.ts
+++ angular/test/src/app/app.component.ts
@@ -12,6 +12,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 
+
 export class AppComponent {
 klikni() {
 window.alert("got you again");
--- angular/test/src/app/comp/comp.component.ts
+++ angular/test/src/app/comp/comp.component.ts
@@ -1,22 +1,55 @@
 import { Component, DirectiveDecorator, Input } from '@angular/core';
 import{driver} from '../../driver';
-
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-comp',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './comp.component.html',
-  styleUrl: './comp.component.css'
+  styleUrls: ['./comp.component.css']
 })
+
 export class CompComponent {
+
+  klasi() {
+    return {
+      'begin': this.vozac?.category === 'ASD', 
+      'adv': this.vozac?.category === 'EXPERT', 
+      'undr': true 
+    };
+  }
+
+  klasi2() {
+    if (this.vozac?.category == 'ASD') {
+      return 'begin';
+    } else {
+      return 'adv'; 
+    }
+  }
+
+  stil(){
+    return { 'text-decoration': 'underline' }; 
+  }
+  
+  onDrvView() {
+    let link: string | undefined;
+    
+    
+    if (this.vozac?.iconUrl) {
+      link = this.vozac?.iconUrl;
+    } else {
+      link = "https://www.google.com"; 
+    }
+    
+    window.open(link, "_blank"); 
+  }
+  
+  
 
   @Input()
   vozac: driver|undefined;
-  onDrvView() {
-   window.alert("got you")
-  }
+
   @Input()
   indeks: number|undefined;
 }
